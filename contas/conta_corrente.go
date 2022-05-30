673683a8b84ec368a3da20772e4746c870855a7f
cledson-leite <csbetsonline@gmail.com>
2022-05-30 14:56:23 -0300

    uso do encapsulamento

--- contas/conta_corrente.go
+++ contas/conta_corrente.go
@@ -7,33 +7,36 @@ type ContaCorrente struct {
 	Titular clientes.Titular
 	NumeroAgencia int
 	NumeroConta int
-	Saldo float64
+	saldo float64
 }
 
 // (conta *ContaCorrente) avisar que essa função usara qualquer variavel do tipo contacorrente como self
+func (conta *ContaCorrente) Saldo() float64 {
+	return conta.saldo
+}
 func (conta *ContaCorrente) Sacar(valor float64) string {
-	if valor > 0 && valor <= conta.Saldo {
-		conta.Saldo -= valor
+	if valor > 0 && valor <= conta.saldo {
+		conta.saldo -= valor
 		return "Saque realizado com sucesso"
 	}
-	return "Saldo insuficiente"
+	return "saldo insuficiente"
 }
 
 // (string, float64) range de retornos obrigatorios no caso de retornos multiplos
 func (conta *ContaCorrente) Depositar(valor float64) (string, float64) {
 	if valor > 0 {
-		conta.Saldo += valor
-		return "deposito realizado com sucesso", conta.Saldo
+		conta.saldo += valor
+		return "deposito realizado com sucesso", conta.saldo
 	}
-	return "Valores incorretos", conta.Saldo
+	return "Valores incorretos", conta.saldo
 }
 func (conta *ContaCorrente) Tranferencia(valor float64, destino *ContaCorrente) (string, float64, float64) {
-	if valor > 0 && valor < conta.Saldo {
+	if valor > 0 && valor < conta.saldo {
 		conta.Sacar(valor)
 		destino.Depositar(valor)
-		return "Transferência realizada com sucesso", conta.Saldo, destino.Saldo
+		return "Transferência realizada com sucesso", conta.saldo, destino.saldo
 	} else if valor < 0 {
-		return "Valores incorretos", valor, conta.Saldo
+		return "Valores incorretos", valor, conta.saldo
 	}
-	return "Saldo insuficiente", valor, conta.Saldo
+	return "saldo insuficiente", valor, conta.saldo
 }
--- main.go
+++ main.go
@@ -12,36 +12,39 @@ import (
 func main()  {
 	cledson := clientes.Titular{Nome: "Cledson", CPF: "1234568901", Profissao: "dev"}
 	roberia := clientes.Titular{Nome: "Roberia", CPF: "1098654321", Profissao: "dev"}
-	contaDoCledson := contas.ContaCorrente{Titular: cledson , NumeroAgencia: 589, NumeroConta: 123456, Saldo: 125.50}
-	contaDaRoberia := contas.ContaCorrente{roberia, 222, 111222, 200}
+	contaDoCledson := contas.ContaCorrente{Titular: cledson , NumeroAgencia: 589, NumeroConta: 123456}
+	contaDoCledson.Depositar(1000)
+	contaDaRoberia := contas.ContaCorrente{Titular: roberia, NumeroAgencia: 222, NumeroConta: 111222}
+	contaDaRoberia.Depositar(1000)
+
 	// o asterisco é um PONTEIRO para o local da memoria
 	var contaDaCleide *contas.ContaCorrente
 	contaDaCleide = new(contas.ContaCorrente)
 	contaDaCleide.Titular.Nome = "Cleide"
-	contaDaCleide.Saldo = 350.90
+	contaDaCleide.Depositar(350.90)
 
-	fmt.Println(contaDoCledson.Saldo)
+	fmt.Println(contaDoCledson.Saldo())
 	
 	fmt.Println(contaDoCledson.Sacar(25.00))
-	fmt.Println(contaDoCledson.Saldo)
+	fmt.Println(contaDoCledson.Saldo())
 
 	fmt.Println(contaDoCledson.Sacar(250.00))
-	fmt.Println(contaDoCledson.Saldo)
+	fmt.Println(contaDoCledson.Saldo())
 
 	fmt.Println(contaDoCledson.Sacar(-25.00))
-	fmt.Println(contaDoCledson.Saldo)
+	fmt.Println(contaDoCledson.Saldo())
 
 	status, valor := contaDoCledson.Depositar(10)
 
 	fmt.Println(status,valor)
 	
-	fmt.Println(contaDoCledson.Saldo, contaDaRoberia.Saldo)
+	fmt.Println(contaDoCledson.Saldo(), contaDaRoberia.Saldo())
 	fmt.Println(contaDoCledson.Tranferencia(50, &contaDaRoberia))
 	
-	fmt.Println(contaDoCledson.Saldo, contaDaRoberia.Saldo)
+	fmt.Println(contaDoCledson.Saldo(), contaDaRoberia.Saldo())
 	fmt.Println(contaDoCledson.Tranferencia(500, &contaDaRoberia))
 	
-	fmt.Println(contaDoCledson.Saldo, contaDaRoberia.Saldo)
+	fmt.Println(contaDoCledson.Saldo(), contaDaRoberia.Saldo())
 	fmt.Println(contaDoCledson.Tranferencia(-50, &contaDaRoberia))
 
 }
